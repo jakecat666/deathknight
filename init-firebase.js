80bf3978f3cbc4e30c5c5f95d698fe0b98a35884
jakecat666 <102033313+jakecat666@users.noreply.github.com>
2022-03-21 18:56:27 +0700

    Add files via upload

---
+++ init-firebase.js
@@ -0,0 +1,13 @@
+  const firebaseConfig = {
+    apiKey: "AIzaSyA1oK-7GOSr_1oXXQ9PBF1_7P9UHGB1KWI",
+    authDomain: "deathknight-5fc47.firebaseapp.com",
+    databaseURL: "https://deathknight-5fc47-default-rtdb.firebaseio.com",
+    projectId: "deathknight-5fc47",
+    storageBucket: "deathknight-5fc47.appspot.com",
+    messagingSenderId: "1076808819919",
+    appId: "1:1076808819919:web:98791739eb223797d5c3ba",
+    measurementId: "G-QDC1444X17"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
